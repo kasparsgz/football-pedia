c0057d9b7d4f6d843fc47ce2a7b33c5008ea716d
kasparsgz <104580852+kasparsgz@users.noreply.github.com>
2022-06-24 14:31:29 +0300

    Add files via upload

---
+++ app/Policies/CountryPolicy.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Country;
+use App\Models\League;
+use App\Models\Player;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CountryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Country $country)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+
+
+
+        //For demo purposes only: we allow creating new Artist(model) objects only to users with ID=1
+        // that is, the first user in the database => myself, the tester ;)
+        return ($user->id == 1)
+            ? Response::allow()
+            : Response::deny("Countries can be added by any user with ID=1 :)");
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Country $country)
+    {
+        //admins may edit all items
+        return ($user->hasRole('admin'))
+        ? Response::allow()
+        : Response::deny("You have to become an admin if you want to edit items!");
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Country $country)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Country $country )
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Country $country)
+    {
+        //
+    }
+}
